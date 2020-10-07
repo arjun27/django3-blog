ba814d39ca55a7d8b77944cbc94ed3f8ddaab37b
Arjun Attam <arjunattam@gmail.com>
2020-10-07 16:33:16 -0700

    update test

--- blog/tests.py
+++ blog/tests.py
@@ -1,17 +1,23 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.contrib.auth.models import User
-from django.conf import settings
+from django.core.management import call_command
 from playwright import sync_playwright
+
+import os
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 class ExampleTests(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        call_command('migrate')
+        call_command('createsuperuser', '--noinput', '--username', 'admin', '--email', 'admin@admin.com')
+        call_command('loaddata', 'data.json')
         cls.playwright = sync_playwright().start()
         cls.browser = cls.playwright.chromium.launch(headless=False)
 
     @classmethod
     def tearDownClass(cls):
+        super().tearDownClass()
         cls.browser.close()
         cls.playwright.stop()
 
@@ -19,5 +25,5 @@ class ExampleTests(StaticLiveServerTestCase):
         page = self.browser.newPage()
         page.goto(self.live_server_url)
         page.click('text=Lorem ipsum 1')
-        url = page.url()
-        assert 'post/1' in url
+        assert 'post/1' in page.url
+        page.close()
